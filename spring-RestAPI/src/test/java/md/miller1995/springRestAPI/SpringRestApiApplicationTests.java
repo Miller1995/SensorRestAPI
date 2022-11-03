08014b45939625a7af8d3236d62be0f2b50b74ee
miller1995 <nircaanton95@gmail.com>
2022-11-03 22:10:47 +0200

    -  created a new maven project spring-RestAPI with Spring Boot; - add dependencies ( Spring Web, Thymeleaf, Spring Data JPA, PostgreSQL Driver, Validation);

---
+++ spring-RestAPI/src/test/java/md/miller1995/springRestAPI/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package md.miller1995.springRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
